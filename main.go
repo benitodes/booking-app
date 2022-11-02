ff70228f31268aa30ddcc52960d3b29936124d1a
Benoit Destresse <benoit.destresse@gmail.com>
2022-11-02 23:33:45 +0800

    added slice of bookings

--- main.go
+++ main.go
@@ -11,26 +11,33 @@ func main() {
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+	var bookings []string
 	var userName string
-	var userTickets int
+	var userTickets uint
+
 	// ask user for their name
 	fmt.Println("Please enter your name")
 	fmt.Scan(&userName)
+
 	// ask user for number of tickets
 	fmt.Println("Please enter number of tickets")
 	fmt.Scan(&userTickets)
+
 	// check if user has entered valid number of tickets
-	if userTickets > int(remainingTickets) {
+	if userTickets > remainingTickets {
 		fmt.Println("Sorry, we don't have that many tickets left")
+
 	} else {
+
 		// update remaining tickets
-		remainingTickets -= uint(userTickets)
+		remainingTickets -= userTickets
 		fmt.Printf("Thank you %v for booking %v tickets\n", userName, userTickets)
 		fmt.Printf("We have %v tickets left\n", remainingTickets)
+
+		bookings = append(bookings, userName)
+		fmt.Printf("this is the slice %v\n", bookings)
 	}
 
-	// userName = "Toma"
-	// userTickets = 2
 	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
 
 }
