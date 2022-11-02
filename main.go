35d200d33448222a89aa47debf74b885fd1d9d32
Benoit Destresse <benoit.destresse@gmail.com>
2022-11-02 23:53:32 +0800

    deleted if statement

--- main.go
+++ main.go
@@ -6,38 +6,30 @@ func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets uint = 50
 	var remainingTickets uint = 50
+	var bookings []string
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var bookings []string
-	var userName string
-	var userTickets uint
+	for {
+		var userName string
+		var userTickets uint
 
-	// ask user for their name
-	fmt.Println("Please enter your name")
-	fmt.Scan(&userName)
+		// ask user for their name
+		fmt.Println("Please enter your name")
+		fmt.Scan(&userName)
 
-	// ask user for number of tickets
-	fmt.Println("Please enter number of tickets")
-	fmt.Scan(&userTickets)
-
-	// check if user has entered valid number of tickets
-	if userTickets > remainingTickets {
-		fmt.Println("Sorry, we don't have that many tickets left")
-
-	} else {
+		// ask user for number of tickets
+		fmt.Println("Please enter number of tickets")
+		fmt.Scan(&userTickets)
 
 		// update remaining tickets
 		remainingTickets -= userTickets
+		bookings = append(bookings, userName)
+
 		fmt.Printf("Thank you %v for booking %v tickets\n", userName, userTickets)
 		fmt.Printf("We have %v tickets left\n", remainingTickets)
-
-		bookings = append(bookings, userName)
-		fmt.Printf("this is the slice %v\n", bookings)
+		fmt.Printf("Bookings so far: %v \n", bookings)
 	}
-
-	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
-
 }
